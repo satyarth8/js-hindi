736072e946846b46016a701568bed918149419e4
Satyartha Prakash <126249188+satyarth8@users.noreply.github.com>
2025-10-12 16:27:22 +0000

    day3

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId =144553
+let accountEmail = "hitesh@google.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+let accountState;
+
+console.table([accountEmail,accountId,accountState])
