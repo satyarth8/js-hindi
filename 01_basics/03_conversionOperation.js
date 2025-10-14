535fcde4189838c11a002e172d9051f18157b809
Satyartha Prakash <126249188+satyarth8@users.noreply.github.com>
2025-10-14 18:33:18 +0000

    day 3

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,12 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log("Value in number ",valueInNumber);
+
+
+let isLoggedIn = true
+// true 1 , false 0
